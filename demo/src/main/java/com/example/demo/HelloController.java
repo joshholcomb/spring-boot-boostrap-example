cc0e8f92b3481d6b41157565899fb1b6ec708273
joshholcomb <60332672+joshholcomb@users.noreply.github.com>
2020-03-02 22:13:33 -0500

    thymeleaf integration
    thymeleaf integration

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
 
-	@RequestMapping("/")
+	@RequestMapping("/hellotest")
 	public String index() {
 		return "Greetings from SpringBoot";
 	}
