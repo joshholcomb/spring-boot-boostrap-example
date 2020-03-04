f97a9493407d7a5ae0aa0a42be9a8fc66e0cc9fc
joshholcomb <60332672+joshholcomb@users.noreply.github.com>
2020-03-03 22:42:06 -0500

    added task list
    added task list

---
+++ demo/src/main/java/com/example/demo/Task.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class Task {
+    private long id;
+    private String task;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+}
--- demo/src/main/java/com/example/demo/WelcomeController.java
+++ demo/src/main/java/com/example/demo/WelcomeController.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
 
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -16,25 +20,26 @@ public class WelcomeController {
     @Value("${welcome.message}")
     private String message;
 
-    private List<String> tasks = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
+    private List<String> tasks = Arrays.asList("task1", "task2", "task3");
+    private List<Task> taskList = new ArrayList<Task>();
 
-    @GetMapping("/")
+    @GetMapping("/welcome")
     public String main(Model model) {
         model.addAttribute("message", message);
-        model.addAttribute("tasks", tasks);
+        model.addAttribute("taskList", taskList);
+        model.addAttribute("task", new Task());
 
         return "welcome"; //view
     }
 
-    // /hello?name=kotlin
-    @GetMapping("/welcome")
-    public String mainWithParam(
-            @RequestParam(name = "name", required = false, defaultValue = "") 
-			String name, Model model) {
-
-        model.addAttribute("message", name);
-
-        return "welcome"; //view
+    @PostMapping("/welcome")
+    public String welcomeSubmit(Model model, @ModelAttribute Task task) {
+        taskList.add(task);
+        int listSize = taskList.size();
+        System.out.println("task list size: " + listSize);
+        model.addAttribute("taskList", taskList);
+        model.addAttribute("task", new Task());
+        return "welcome";
     }
 
 }
